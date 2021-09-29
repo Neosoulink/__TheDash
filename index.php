efadeb3c33f2eaa0a87511f2960fc483b9d120e6
Neosoulink <neodarksoulink@gmail.com>
2021-09-29 17:31:40 +0100

    Update App structure

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require('./.__localDashboard__/lib/app/controllers/Autoloader.class.php');
+require('./controllers/Autoloader.class.php');
 
 Autoloader::register();
 SessionManager::ss();
 
 // IMPORTANT: DEFINE A ROOTER !!!
 App::launchApp();
+//
