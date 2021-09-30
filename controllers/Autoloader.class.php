70ac19bf19fd7d916ef40f4a1f93b66f40af2b04
Neosoulink <neodarksoulink@gmail.com>
2021-09-30 14:04:38 +0100

    Resolev dotenv import

--- controllers/Autoloader.class.php
+++ controllers/Autoloader.class.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Controllers;
+
 /**
  * App autoloader.
  *
--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require(__DIR__ . '/controllers/Autoloader.class.php');
+
+use Controllers\Autoloader;
 
 Autoloader::register();
 SessionManager::ss();
