70b716f19260bf842f17c4c9b7b8a8a6bfabbf91
Neosoulink <neodarksoulink@gmail.com>
2021-10-04 00:06:18 +0100

    Init layout

---
+++ components/Layout.php
@@ -0,0 +1,25 @@
+<?php
+function layout(string $children = ""): string
+{
+	$layout = `<!DOCTYPE html>
+		<html lang="en">
+
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+			<title>__TheDash</title>
+
+			<link rel="stylesheet" href="` . Helpers::getAssetsPath() . `/css/index.css">
+		</head>
+
+		<body>
+		` . $children . `
+		</body>
+
+		</html>
+	`;
+
+	return $layout;
+}
