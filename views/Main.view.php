7b283c2c16c4391c29830c05b6fd6fe34ed1b559
Neosoulink <neodarksoulink@gmail.com>
2021-10-09 17:57:29 +0100

    Update sidebar style

--- views/Main.view.php
+++ views/Main.view.php
@@ -2,38 +2,12 @@
 <input id="JsonProjectList" type="hidden" value='<?= $JsonProjectList ?>'>
 
 <div id="app">
-	<header class="nav space-between-centered">
-		<h1 class="app-name">myProjects</h1>
-		<div class="">
-			<button class="">Favoris</button>
+	<section id="sidebar-menu">
+		<div class="logo-container">
+			<h1 class="text-logo">_TheDash</h1>
 		</div>
-	</header>
-	<main class="body fill">
-		<h1 class="body-title">My local projects {{ projectList?.length }}</h1>
-
-		<section v-if="projectList?.length" class="project-list fill">
-			<div v-for="(projectData, index) in projectList" :key="index" class="project-item space-between-centered">
-				<div>
-					<h2 class="project-name">
-						{{ projectData?.name }} /
-						{{ projectData?.builded_lang }} /
-						Size : {{ projectData?.project_url }}kb
-					</h2>
-					<a :href="projectData?.project_url" target="blank_">Open project</a>
-				</div>
-				<diV>
-					<button>fav</button>
-				</diV>
-			</div>
-		</section>
-		<div v-else class="empty-list centered">
-			List empty
-		</div>
-	</main>
-	<footer class="space-between-centered">
-		<div class="copyright">myProjects@<span id="currentYear">2021</span> | Github</div>
-		<div> Php v7.4.3</div>
-	</footer>
+	</section>
+	<main id="main">d</main>
 </div>
 
 <script src="<?= Helpers::getAssetsPath() ?>/js/Vue3.js"></script>
