bbdde2b316abb390300a1d944908dd67611ab4db
Neosoulink <neodarksoulink@gmail.com>
2021-10-10 00:36:14 +0100

    Minor

--- views/Main.view.php
+++ views/Main.view.php
@@ -8,9 +8,19 @@
 		</div>
 
 		<ul class="nav-container">
-			<a href="#" class="list-item">
+			<a href="#" class="list-item selected">
 				<span class="material-icons">format_list_bulleted</span>
-				<span class="list-item-label">Project list</span>
+				<span class="list-item-label selected">Project list</span>
+			</a>
+
+			<a href="#" class="list-item">
+				<span class="material-icons">favorite_border</span>
+				<span class="list-item-label selected">Favoris</span>
+			</a>
+
+			<a href="#" class="list-item">
+				<span class="material-icons">settings</span>
+				<span class="list-item-label selected">Settings</span>
 			</a>
 		</ul>
 	</section>
