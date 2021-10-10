774802c8df301e1322dd6ec08d4fda8315777d67
Neosoulink <neodarksoulink@gmail.com>
2021-10-10 01:27:37 +0100

    Update left sidbar

--- views/Main.view.php
+++ views/Main.view.php
@@ -8,12 +8,19 @@
 		</div>
 
 		<ul class="nav-container">
-
 			<a href="#" class="list-item" v-for="(item, index) in sidebarMenuList" :key="index" :class="((!currentModal || currentModal === null) && index === 0) || (currentModal === item?.label) ? 'selected' : ''" @click="currentModal = item?.label">
 				<span class="material-icons">{{ item?.icon }}</span>
 				<span class="list-item-label selected">{{ item?.label }}</span>
 			</a>
 		</ul>
+
+		<div class="gift-card-container">
+			<a href="https://github.com/Neosoulink" target="blank_" class="gift-card">
+				<span class="material-icons">redeem</span>
+				<h2>Gift</h2>
+				<p>Follow the instructions to embed the icon font in your</p>
+			</a>
+		</div>
 	</section>
 	<main id="main">d</main>
 </div>
