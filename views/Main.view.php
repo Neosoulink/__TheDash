e9f982edc9f326c70c0e894cce159e0da06ecd2d
Neosoulink <neodarksoulink@gmail.com>
2021-11-07 21:24:09 +0100

    Minor fix

--- views/Main.view.php
+++ views/Main.view.php
@@ -13,7 +13,7 @@
 			</div>
 
 			<a href="#" title="Support the creator on github" class="btn btn-circle-icon"><span class="material-icons icon">redeem</span></a>
-			<a href="https://github.com/Neosoulink/__TheDash" target="blank_" title="See github repo" class="btn btn-circle-icon"><img src="php Helpers::getAssetsPath() ?>/svg/logos/github.svg" class="icon" /></a>
+			<a href="https://github.com/Neosoulink/__TheDash" target="blank_" title="See github repo" class="btn btn-circle-icon"><img src="<?= Helpers::getAssetsPath() ?>/svg/logos/github.svg ?>" class="icon" /></a>
 		</header>
 		<main>
 
