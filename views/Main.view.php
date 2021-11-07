98e154f18eaa954dea8401924391794406b8d27f
Neosoulink <neodarksoulink@gmail.com>
2021-11-07 21:40:16 +0100

    Remove modal

--- views/Main.view.php
+++ views/Main.view.php
@@ -27,18 +27,6 @@
 
 	</section>
 
-	<div class="modal" :class="{visible: showModal}" @click.self="currentModal = null">
-		<div class="card-content">
-			<button class="btn btn-light btn-circle-icon close-btn" @click="currentModal = null">
-				<span class="material-icons icon">close</span>
-			</button>
-			<h3 class="card-title">Favoris</h3>
-
-			<div class="card-body">
-
-			</div>
-		</div>
-	</div>
 </div>
 
 <!-- SCRIPTS -->
