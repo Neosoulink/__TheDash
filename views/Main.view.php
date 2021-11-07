20d09e28e96f68dd4ea1c61c26ac2634748faea7
Neosoulink <neodarksoulink@gmail.com>
2021-11-07 21:17:52 +0100

    Create projectList routeView component

--- views/Main.view.php
+++ views/Main.view.php
@@ -3,7 +3,7 @@
 
 <div id="app">
 
-	<?php require(Helpers::getRelativeRootPath() . "/components/dashboard/SideBar.php") ?>
+	<?= require(Helpers::getRelativeRootPath() . "/components/dashboard/SideBar.php") ?>
 
 	<section id="main">
 		<header class="main-header">
@@ -16,52 +16,8 @@
 			<a href="https://github.com/Neosoulink/__TheDash" target="blank_" title="See github repo" class="btn btn-circle-icon"><img src="<?= Helpers::getAssetsPath() ?>/svg/logos/github.svg" class="icon" /></a>
 		</header>
 		<main>
-			<div class="header-title-container">
-				<h2>Project list</h2>
 
-				<div>
-
-				</div>
-				<select class="input">
-					<option>Order by</option>
-					<option v-for="(label, index) in orderByOptions" :key="index">label</option>
-				</select>
-			</div>
-
-			<div class="cards-projects-container">
-				<div v-for="(item, index) in projectListFiltered" :key="index" class="card-project">
-					<div class="card-img">
-						<img src="<?= Helpers::getAssetsPath() ?>/img/computer-programming.png" />
-					</div>
-					<div class="card-body">
-						<h4 class="title">{{ item?.name }}</h4>
-						<div class="content">
-							<ul>
-								<li>Size : {{ item?.size }}kb</li>
-								<li>Permissions : {{ item?.permissions }}</li>
-							</ul>
-							<ul>
-								<li>Last access: {{ new Date(item?.last_access_time).toString().substr(0, 16) }}</li>
-								<li>Updated at: {{ new Date(item?.modification_time).toString().substr(0, 16) }}</li>
-							</ul>
-						</div>
-						<div class="footer">
-							<ul>
-								<li>
-									<img class="img-icon" v-if="item?.builded_lang != 'Unknown'" :src="`<?= Helpers::getAssetsPath() ?>/svg/builded_env/${item?.builded_lang?.toLowerCase()}.svg`" />
-								</li>
-							</ul>
-							<div>
-								<a :href="item?.project_url" target="blank_" class="btn btn-light btn-circle-icon"><span class="text-accent material-icons icon">visibility</span></a>
-
-								<a href="#" target="blank_" class="btn btn-light btn-circle-icon"><span class="text-accent material-icons icon">edit</span></a>
-
-								<a href="#" target="blank_" class="btn btn-light btn-circle-icon"><span class="text-accent material-icons icon">favorite_border</span></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?= require(Helpers::getRelativeRootPath() . "/components/dashboard/routeViews/projectList.php") ?>
 
 		</main>
 		<footer>
