eb880f180d5fc484c3aa577e2320d64c1ab7aa1a
Neosoulink <neodarksoulink@gmail.com>
2021-11-10 02:04:31 +0100

    Fix favorites loop

--- components/dashboard/routeViews/Favoris.php
+++ components/dashboard/routeViews/Favoris.php
@@ -2,3 +2,9 @@
 	<h2>Favoris</h2>
 
 </div>
+
+<div class="cards-projects-container">
+	<div v-for="(item, index) in projectListFiltered.filter(i => favoritesProjects.includes(i.name))" :key="index">
+		<?php require(Helpers::getRelativeRootPath() . '/components/dashboard/cards/CardProject.php') ?>
+	</div>
+</div>
