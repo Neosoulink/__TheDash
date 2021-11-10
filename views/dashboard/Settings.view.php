df771c0d51a1811c989176851f8c38f2f50608f8
Neosoulink <neodarksoulink@gmail.com>
2021-11-10 15:15:33 +0100

    Minor ui update

--- views/dashboard/Settings.view.php
+++ views/dashboard/Settings.view.php
@@ -4,4 +4,13 @@
 </div>
 
 <div class="cards-projects-container">
+	<div class="option">
+		<h3 class="option-title">Active virtual host</h3>
+		<p>Sometime you can specify that you've an virtual host</p>
+	</div>
+
+	<div class="option-control">
+		<input type="checkbox" />
+	</div>
+
 </div>
