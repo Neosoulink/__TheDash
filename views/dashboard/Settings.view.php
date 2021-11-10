5be718e6082b76907fe684e7052578a73d7ae1b7
Neosoulink <neodarksoulink@gmail.com>
2021-11-10 15:58:06 +0100

    Design option-items

--- views/dashboard/Settings.view.php
+++ views/dashboard/Settings.view.php
@@ -4,13 +4,28 @@
 </div>
 
 <div class="cards-projects-container">
-	<div class="option">
-		<h3 class="option-title">Active virtual host</h3>
-		<p>Sometime you can specify that you've an virtual host</p>
+	<div class="option-item">
+		<div>
+			<h3 class="option-title">Active virtual host</h3>
+			<p class="option-detail">Sometime you can specify that you've an virtual host</p>
+		</div>
+
+		<div class="option-control">
+			<input type="checkbox" />
+			<span class="checkmark"></span>
+		</div>
 	</div>
 
-	<div class="option-control">
-		<input type="checkbox" />
+	<div class="option-item disabled">
+		<div>
+			<h3 class="option-title">Virtual host domain</h3>
+			<p class="option-detail">Update your local domain. (e.g: myProject.test)</p>
+		</div>
+
+		<div class="option-control">
+			<input type="text" value="test" class="input" style="background-color: white;border: 1px solid #00000050;" />
+		</div>
 	</div>
+
 
 </div>
