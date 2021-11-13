ed4da9dc2e4f1bc9b2392f0bdc48d418a818cbd8
Neosoulink <neodarksoulink@gmail.com>
2021-11-13 02:38:15 +0100

    Set empty state

--- views/dashboard/Favoris.view.php
+++ views/dashboard/Favoris.view.php
@@ -3,8 +3,14 @@
 
 </div>
 
-<div class="cards-projects-container">
+<div class="cards-projects-container" v-if="favorisProjectListFiltered.length">
 	<div v-for="(item, index) in favorisProjectListFiltered" :key="index">
 		<?php require(Helpers::getRelativeRootPath() . '/components/dashboard/cards/CardProject.php') ?>
 	</div>
+</div>
+
+<div class="empty-projects-container" v-else>
+	<img src="<?= Helpers::getRelativeRootPath() ?>/assets/svg/no-items.svg" />
+
+	<h3 class="text-muted">No favorites</h3>
 </div>
--- views/dashboard/ProjectList.view.php
+++ views/dashboard/ProjectList.view.php
@@ -10,8 +10,14 @@
 	</select>
 </div>
 
-<div class="cards-projects-container">
+<div class="cards-projects-container" v-if="projectListFiltered.length">
 	<div v-for="(item, index) in projectListFiltered" :key="index">
 		<?php require(Helpers::getRelativeRootPath() . '/components/dashboard/cards/CardProject.php') ?>
 	</div>
+</div>
+
+<div class="empty-projects-container" v-else>
+	<img src="<?= Helpers::getRelativeRootPath() ?>/assets/svg/no-items.svg" />
+
+	<h3 class="text-muted">No projects</h3>
 </div>
