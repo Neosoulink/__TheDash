03cf788035b232943da90e841cbb03cc4106fc64
Neosoulink <neodarksoulink@gmail.com>
2021-11-13 04:21:07 +0100

    Update card design

--- components/dashboard/cards/CardProject.php
+++ components/dashboard/cards/CardProject.php
@@ -1,7 +1,9 @@
 <div class="card-project">
 	<a :href="formatProjectLink(item)" :target="optionItems.openWithBlank ? 'blank_' : '' " class="card-img">
-		<img src="<?= Helpers::getAssetsPath() ?>/img/computer-programming.png" />
+		<img :src="`<?= Helpers::getAssetsPath() ?>/svg/builded_env/${item?.framework?.toLowerCase()}.svg`" />
 	</a>
+	<!-- /.img -->
+
 	<div class="card-body">
 		<h4 class="title">{{ item?.name }}</h4>
 		<div class="content">
@@ -14,22 +16,19 @@
 				<li>Updated at : {{ item?.modification_time }}</li>
 			</ul>
 		</div>
-		<div class="footer">
-			<ul>
-				<li>
-					<img class="img-icon" :src="`<?= Helpers::getAssetsPath() ?>/svg/builded_env/${item?.framework?.toLowerCase()}.svg`" />
-				</li>
-			</ul>
-			<div>
-				<a :href="formatProjectLink(item)" :target="optionItems.openWithBlank ? 'blank_' : '' " class="btn btn-light btn-circle-icon"><span class="text-accent material-icons icon">visibility</span></a>
-
-				<!-- TODO: Add edit feature -->
-				<!--<a href="#" target="blank_" class="btn btn-light btn-circle-icon" @click.prevent="alert('Available soon')"><span class="text-accent material-icons icon">edit</span></a>-->
-
-				<a href="#" target="blank_" class="btn btn-light btn-circle-icon" :class="{favorite : isFavorite(item?.name)}" @click.prevent="!isFavorite(item?.name) ? addFavorite(item?.name) : removeFavorite(item?.name)">
-					<span class="text-accent material-icons icon">{{isFavorite(item?.name) ? 'favorite' : 'favorite_border'}}</span>
-				</a>
-			</div>
-		</div>
 	</div>
+	<!-- /.card-body -->
+
+	<div class="card-footer">
+		<a :href="formatProjectLink(item)" :target="optionItems.openWithBlank ? 'blank_' : '' " class="btn btn-light btn-circle-icon"><span class="text-accent material-icons icon">visibility</span></a>
+
+		<!-- TODO: Add edit feature -->
+		<!--<a href="#" target="blank_" class="btn btn-light btn-circle-icon" @click.prevent="alert('Available soon')"><span class="text-accent material-icons icon">edit</span></a>-->
+
+		<a href="#" target="blank_" class="btn btn-light btn-circle-icon" :class="{favorite : isFavorite(item?.name)}" @click.prevent="!isFavorite(item?.name) ? addFavorite(item?.name) : removeFavorite(item?.name)">
+			<span class="text-accent material-icons icon">{{isFavorite(item?.name) ? 'favorite' : 'favorite_border'}}</span>
+		</a>
+	</div>
+	<!-- /.card-footer -->
+
 </div>
